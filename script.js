cf22fbf7324200f687d0123b1cfc9de96f48f39e
Justin Vos <justinvosnz@gmail.com>
2019-08-05 18:04:14 +1200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function handleSubmit () {
+  const textField = document.getElementById('textField')
+
+  textField.
+
+  console.log('handleSubmit')
+}
+
+document.getElementById('submitBtn').addEventListener('click', handleSubmit)
